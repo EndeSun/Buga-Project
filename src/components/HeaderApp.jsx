fe55163bf8fcc2a6adc47e4ededed1122e84406d
ende <1195562121ende@gmail.com>
2023-08-10 22:00:46 +0200

    Jugando con el header

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -10,13 +10,15 @@ function HeaderApp() {
     <>
       <Navbar fluid className="bg-neutral-950 w-screen">
         {/* Logo con el link del logo */}
-        <Link className="no-underline" to="/">
+        <Link className="no-underline hover:brightness-125" to="/">
             <img
               alt="Logo de Buga Ramen"
               src="../src/assets/logo.png"
               width="80"
             />
         </Link>
+
+        <p className="Zaragoza-head md:hidden mt-4 text-slate-200 text-3xl bg-transparent">Buga Ramen</p>
 
         {/* Logo del menú */}
         <div className="flex md:order-2">
@@ -25,12 +27,12 @@ function HeaderApp() {
 
         {/* Los  Elementos del menú*/}
         <Navbar.Collapse>
-          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Entrantes</Link>
-          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Ramen</Link>
-          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Don Style</Link>
-          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Combos/Especiales</Link>
-          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none md:hidden">Idiomas</Link>
-          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none md:hidden">Contacto</Link>
+          <Link to="/Entrantes" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Entrantes</Link>
+          <Link to="/Ramen" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Ramen</Link>
+          <Link to="/Don" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Don Style</Link>
+          <Link to="/Combos" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Combos/Especiales</Link>
+          <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:borderc-none md:hidden">Idiomas</Link>
+          <Link to="/FormularioContacto" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none md:hidden">Contacto</Link>
         </Navbar.Collapse>
       </Navbar>
     </>
