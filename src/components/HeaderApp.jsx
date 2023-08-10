6063b12372612f589eefc206b7992289308ae6f1
ende <1195562121ende@gmail.com>
2023-08-11 00:53:55 +0200

    Subiendo los archivos svg de los alérgenos

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -18,19 +18,19 @@ function HeaderApp() {
             />
         </Link>
 
-        <p className="Zaragoza-head md:hidden mt-4 text-slate-200 text-3xl bg-transparent">Buga Ramen</p>
-
         {/* Logo del menú */}
         <div className="flex md:order-2">
-          <Navbar.Toggle />
+          <Navbar.Toggle/>
         </div>
+
+        
 
         {/* Los  Elementos del menú*/}
         <Navbar.Collapse>
           <Link to="/Entrantes" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Entrantes</Link>
           <Link to="/Ramen" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Ramen</Link>
           <Link to="/Don" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Don Style</Link>
-          <Link to="/Combos" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Combos/Especiales</Link>
+          <Link to="/Combos" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none">Combos</Link>
           <Link to="/" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:borderc-none md:hidden">Idiomas</Link>
           <Link to="/FormularioContacto" className="navBar2 no-underline bg-neutral-950 text-neutral-200 text-xl font-bold hover:duration-500 hover:text-amber-200 my-2 border-b-2 border-b-red-950 md:text-4xl md:border-none md:hidden">Contacto</Link>
         </Navbar.Collapse>
