336ad1a390eca2e19b20a204ddb9e413484b5855
ende <1195562121ende@gmail.com>
2023-08-11 17:24:04 +0200

    Añadiendo los svg en formato de componentes

---
+++ src/components/Allergen/Azufre.jsx
@@ -0,0 +1,252 @@
+import React from "react";
+
+function Azufre() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="262.205"
+      height="372.047"
+      version="1.1"
+    >
+      <g transform="translate(0 -680.315)">
+        <g fillOpacity="1" transform="translate(-784.273 -540.33) scale(4.103)">
+          <text
+            x="223.008"
+            y="364.433"
+            style={{
+              WebkitTextAlign: "center",
+              textAlign: "center",
+              lineHeight: "125%",
+            }}
+            fill="#000"
+            stroke="none"
+            fontFamily="Sans"
+            fontSize="15"
+            fontStyle="normal"
+            fontWeight="normal"
+            letterSpacing="0"
+            textAnchor="middle"
+            wordSpacing="0"
+            xmlSpace="preserve"
+          >
+            <tspan x="223.008" y="364.433" fontSize="5.5">
+              DIÓXIDO DE AZUFRE
+            </tspan>
+            <tspan x="223.008" y="371.308" fontSize="5.5">
+              Y SULFITOS
+            </tspan>
+          </text>
+          <g>
+            <path
+              fill="#82104e"
+              stroke="#82104e"
+              strokeDasharray="none"
+              strokeDashoffset="0"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeMiterlimit="4"
+              strokeOpacity="1"
+              strokeWidth="3"
+              d="M39.937 29.288c0 10.339-8.222 18.72-18.364 18.72-10.142 0-18.364-8.381-18.364-18.72 0-10.34 8.222-18.72 18.364-18.72 10.142 0 18.364 8.38 18.364 18.72z"
+              transform="matrix(1.04033 0 0 1.02051 200.656 305.053)"
+            ></path>
+            <g transform="translate(-290.436 304.96)">
+              <g
+                fill="#fff"
+                stroke="none"
+                fontFamily="Sans"
+                fontSize="40"
+                fontStyle="normal"
+                fontWeight="normal"
+                letterSpacing="0"
+                wordSpacing="0"
+              >
+                <text
+                  xmlSpace="preserve"
+                  style={{ lineHeight: "125%" }}
+                  x="504.791"
+                  y="23.955"
+                >
+                  <tspan
+                    x="504.791"
+                    y="23.955"
+                    style={{ InkscapeFontSpecification: "Sans Bold" }}
+                    fill="#fff"
+                    fillOpacity="1"
+                    fontSize="10"
+                    fontWeight="bold"
+                  >
+                    E
+                  </tspan>
+                </text>
+                <text
+                  xmlSpace="preserve"
+                  style={{ lineHeight: "125%" }}
+                  x="510.72"
+                  y="24.006"
+                >
+                  <tspan
+                    x="510.72"
+                    y="24.006"
+                    style={{ InkscapeFontSpecification: "Sans Bold" }}
+                    fill="#fff"
+                    fillOpacity="1"
+                    fontSize="10"
+                    fontWeight="bold"
+                  >
+                    -
+                  </tspan>
+                </text>
+                <text
+                  xmlSpace="preserve"
+                  style={{ lineHeight: "125%" }}
+                  x="513.887"
+                  y="23.955"
+                >
+                  <tspan
+                    x="513.887"
+                    y="23.955"
+                    style={{ InkscapeFontSpecification: "Sans Bold" }}
+                    fill="#fff"
+                    fillOpacity="1"
+                    fontSize="10"
+                    fontWeight="bold"
+                  >
+                    X
+                  </tspan>
+                </text>
+              </g>
+              <path
+                fill="#fff"
+                stroke="#fff"
+                strokeDasharray="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M519.66 43.213l-.019-1.26 6.03-3.583-.076-7.437-5.992-3.393-.016-1.377-.918-.003v1.35l-5.178 3.018-5.272-3.053.022-1.29-.877-.016v1.29l-5.984 3.43-.052 7.576 6.03 3.49-.003 1.23.91.033-.008-1.273 5.156-3.037 5.253 2.993-.038 1.337z"
+              ></path>
+              <path
+                fill="#82104e"
+                stroke="#82104e"
+                strokeDasharray="none"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M507.849 29.12l4.803 2.78.013 5.554-4.874 2.911-4.878-2.814.022-5.711z"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#fff"
+                strokeDasharray="none"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M503.851 32.183l.477.802 3.71-2.156-.447-.792z"
+              ></path>
+              <path
+                fill="#82104e"
+                stroke="#82104e"
+                strokeDasharray="none"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M519.275 28.82l4.803 2.78.013 5.555-4.874 2.91-4.878-2.813.022-5.712z"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#fff"
+                strokeDasharray="none"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M515.197 32.174l.476.802 3.711-2.156-.447-.792z"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#fff"
+                strokeDasharray="none"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M522.397 32.566l.887.017-.018 4.487-3.918 2.322-.444-.815 3.476-1.968z"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#fff"
+                strokeDasharray="none"
+                strokeLinecap="butt"
+                strokeLinejoin="miter"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.05"
+                d="M511.005 32.557l.975.018-.018 4.486-3.919 2.323-.514-.763 3.476-1.986z"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#82104e"
+                strokeDasharray="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.779"
+                d="M501.389 28.693c0 .731-.608 1.324-1.358 1.324-.75 0-1.357-.593-1.357-1.324 0-.732.608-1.324 1.357-1.324.75 0 1.358.592 1.358 1.324z"
+                transform="matrix(1.01493 0 0 1.0402 -5.206 1.747)"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#82104e"
+                strokeDasharray="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.779"
+                d="M501.389 28.693c0 .731-.608 1.324-1.358 1.324-.75 0-1.357-.593-1.357-1.324 0-.732.608-1.324 1.357-1.324.75 0 1.358.592 1.358 1.324z"
+                transform="matrix(1.01493 0 0 1.0402 -5.27 7.98)"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#82104e"
+                strokeDasharray="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.779"
+                d="M501.389 28.693c0 .731-.608 1.324-1.358 1.324-.75 0-1.357-.593-1.357-1.324 0-.732.608-1.324 1.357-1.324.75 0 1.358.592 1.358 1.324z"
+                transform="matrix(1.01493 0 0 1.0402 5.999 8.284)"
+              ></path>
+              <path
+                fill="#fff"
+                stroke="#82104e"
+                strokeDasharray="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeMiterlimit="4"
+                strokeOpacity="1"
+                strokeWidth="0.779"
+                d="M501.389 28.693c0 .731-.608 1.324-1.358 1.324-.75 0-1.357-.593-1.357-1.324 0-.732.608-1.324 1.357-1.324.75 0 1.358.592 1.358 1.324z"
+                transform="matrix(1.01493 0 0 1.0402 17.348 8.152)"
+              ></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Azufre;
