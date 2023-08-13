04d1e9a0b38dedb866b88495d436c8cd65706037
ende <1195562121ende@gmail.com>
2023-08-13 23:48:14 +0200

    Añadiendo estilos de fuentes, modificando el header y las targetas de comidas

--- src/pages/Carta/Entrantes.jsx
+++ src/pages/Carta/Entrantes.jsx
@@ -4,7 +4,7 @@ import Comida from "../../components/Comida";
 function Entrantes() {
   return (
     <>
-      <h1 className='text-center font-bold text-4xl text-amber-500'>Entrantes</h1>
+      <h1 className='text-center font-bold text-4xl text-amber-500 mt-8'>Entrantes</h1>
       <Comida
         source="../src/assets/Entrantes/Ebi furai.png"
         foodName="Ebi Furai"
@@ -12,8 +12,18 @@ function Entrantes() {
         foodPrice="12.8"
         gluten={true}
         crustaceos={true}
-        apio={true}
+        moluscos={true}
+        pescado={true}
       />
+      
+      <Comida
+        source="../src/assets/Entrantes/Edamame.png"
+        foodName="Edamame"
+        foodDescription="Algas en salsa Soja"
+        foodPrice="10.4"
+        soja={true}
+      />
+      <hr className="my-8"/>
     </>
   );
 }
