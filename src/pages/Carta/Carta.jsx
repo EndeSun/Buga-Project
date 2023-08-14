74abd472a04961bb4adab67c0a0eeb19cbcfeb31
ende <1195562121ende@gmail.com>
2023-08-14 20:18:34 +0200

    Modificaciones en la carta

--- src/pages/Carta/Carta.jsx
+++ src/pages/Carta/Carta.jsx
@@ -3,9 +3,27 @@ import React from 'react'
 
 function Carta() {
   return (
-    <div>Carta
-      
+    <>
+
+    <div className='grid grid-cols-2'>
+      {/* section main header footer nav article...  super importante, los contenedores a disponer tienen que estar en un contenedor padre*/}
+      <section className="bg-slate-800 w-18 h-18 m-4 text-white ">
+        <ul className='flex justify-around pt-2'>
+          <li>1.1</li>
+          <li>1.2</li>
+          <li>1.3</li>
+        </ul>
+      </section>
+      <section className="bg-slate-800 w-18 h-18 m-4 text-white">2</section>
     </div>
+
+    <div className='grid grid-cols-3'>
+      <section className="bg-slate-800 w-18 h-18 m-4 text-white col-span-2 col-start-2">3</section>
+      <section className="bg-slate-800 w-18 h-18 m-4 text-white">4</section>
+      <section className="bg-slate-800 w-18 h-18 m-4 text-white">5</section>
+    </div>
+
+    </>
   )
 }
 
