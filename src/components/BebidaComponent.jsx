e78feb8c6506034f26f9d393ef027e5565fcbb5d
pjtc28 <133893146+pjtc28@users.noreply.github.com>
2023-08-21 17:47:34 +0200

    prueba

---
+++ src/components/BebidaComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BebidaComponent() {
+  return (
+    <div>BebidaComponent</div>
+  )
+}
+
+export default BebidaComponent
--- src/components/Descargable.jsx
+++ src/components/Descargable.jsx
@@ -1,1 +1,10 @@
 // PDF descargable que irá debajo de la carta.
+import React from 'react'
+
+function Descargable() {
+  return (
+    <div>Descargable</div>
+  )
+}
+
+export default Descargable
