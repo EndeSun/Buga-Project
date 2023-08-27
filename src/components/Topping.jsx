c36c0d67f56d8cac44c9506934cfc5933b636807
ende <1195562121ende@gmail.com>
2023-08-27 23:35:12 +0200

    Imágenes modificadas con el tamaño adecuado en png, faltan solo los entrantes excluyendo las gyozas y el karaage

--- src/components/Topping.jsx
+++ src/components/Topping.jsx
@@ -6,96 +6,124 @@ function Topping() {
   const [showToppings, setShowToppings] = useState(false);
   return (
     <>
-      <motion.button
-        className="fixed bottom-6 left-6 border-2 bg-amber-300 rounded-xl text-lg p-2 border-black font-bold font-caprasimo uppercase hover:scale-105"
+      <button
+        className="fixed bottom-6 left-6 border-2 bg-amber-300 rounded-xl text-md p-2 border-black font-bold font-caprasimo uppercase"
         onClick={() => {
           setShowToppings(!showToppings);
         }}
       >
-      ➕ Agregar Topping
-      </motion.button>
-      
+        ➕ Agregar Topping
+      </button>
+
       <AnimatePresence>
         {showToppings && (
           <motion.div
-            className="overflow-x-auto whitespace-nowrap w-full h-54 bg-slate-700 text-white fixed bottom-0"
+            className="overflow-x-auto whitespace-nowrap w-full h-54 bg-slate-700 text-white fixed bottom-0 font-caprasimo"
             animate={{ y: [60, 0] }}
-            exit={{ y: [0, 60], opacity: [1,0]}}
+            exit={{ y: [0, 60], opacity: [1, 0] }}
             transition={{ duration: 0.3 }}
           >
-            <div className="flex flex-row [&>div]:flex [&>div]:flex-col [&>div]:items-center [&>div]:my-2 [&>div]:mx-6 [&>div]:p-1 [&>div]:border-1" onClick={()=>{setShowToppings(!showToppings)}}>
+            <div
+              className="flex flex-row [&>div]:flex [&>div]:flex-col [&>div]:items-center [&>div]:my-2 [&>div]:mx-6 [&>div]:p-1 [&>div]:border-1"
+              onClick={() => {
+                setShowToppings(!showToppings);
+              }}
+            >
               <div>
-                <p>Huevo cocido marinado 1,50 €</p>
+                <p>
+                  Huevo cocido marinado{" "}
+                  <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
                 <img
                   src="../src/assets/Toppings/huevo cocido marinado.png"
                   width="60"
                 ></img>
               </div>
               <div>
-                <p>Huevo Hervido 1,50 €</p>
+                <p>
+                  Huevo Hervido{" "}
+                  <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
                 <img
                   src="../src/assets/Toppings/huevo hervido.png"
                   width="60"
                 ></img>
               </div>
               <div>
-                <p>Bambú 1,50 €</p>
-                <img
-                  src="../src/assets/Toppings/bambu.png"
-                  width="40"
-                ></img>
+                <p>
+                  Bambú <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
+                <img src="../src/assets/Toppings/bambu.png" width="40"></img>
               </div>
               <div>
-                <p>Alga Nori 1,50 €</p>
+                <p>
+                  Alga Nori{" "}
+                  <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
                 <img
                   src="../src/assets/Toppings/alga nori.png"
                   width="60"
                 ></img>
               </div>
               <div>
-                <p>Cebollino 1,50 €</p>
+                <p>
+                  Cebollino{" "}
+                  <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
                 <img
                   src="../src/assets/Toppings/cebollino.png"
                   width="60"
                 ></img>
               </div>
               <div>
-                <p>Verduras 1,50 €</p>
-                <img
-                  src="../src/assets/Toppings/verduras.png"
-                  width="60"
-                ></img>
+                <p>
+                  Verduras{" "}
+                  <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
+                <img src="../src/assets/Toppings/verduras.png" width="60"></img>
               </div>
               <div>
-                <p>Ramen 2,50 €</p>
-                <img
-                  src="../src/assets/Toppings/ramen.png"
-                  width="60"
-                ></img>
+                <p>
+                  Ramen <span className="text-red-500 font-bold">2,50 €</span>
+                </p>
+                <img src="../src/assets/Toppings/ramen.png" width="60"></img>
               </div>
               <div>
-                <p>Wantang (5u) 2,50 €</p>
-                <img
-                  src="../src/assets/Toppings/wangtang.png"
-                  width="60"
-                ></img>
+                <p>
+                  Wantang (5u){" "}
+                  <span className="text-red-500 font-bold">2,50 €</span>
+                </p>
+                <img src="../src/assets/Toppings/wangtang.png" width="60"></img>
               </div>
               <div>
-                <p>Naruto 1,50 €</p>
-                <img
-                  src="../src/assets/Toppings/naruto.png"
-                  width="60"
-                ></img>
+                <p>
+                  Naruto <span className="text-red-500 font-bold">1,50 €</span>
+                </p>
+                <img src="../src/assets/Toppings/naruto.png" width="60"></img>
               </div>
-              
             </div>
             <button
-            className="fixed border-2 p-1 bottom-2 left-2 bg-slate-400 text-red-800 rounded-lg"
+              className="fixed p-1 m-2 bottom-2 left bg-white text-red-800 rounded-lg"
               onClick={() => {
                 setShowToppings(!showToppings);
               }}
             >
-              Cerrar
+              <motion.svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+                whileHover={{ rotate: 180 }}
+                transition={{ duration: 0.1 }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </motion.svg>
             </button>
           </motion.div>
         )}
