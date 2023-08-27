17a258d13e5a3a212831388dd433196b237a9f8c
ende <1195562121ende@gmail.com>
2023-08-27 12:55:33 +0200

    Añadido los platos donbury

--- src/pages/Carta/Don.jsx
+++ src/pages/Carta/Don.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+import Comida from "../../components/Comida";
 
 function Don() {
   return (
     <>
-    
+      <h1 className="text-center font-extrabold font-caprasimo text-3xl text-amber-600 my-6 drop-shadow-md ">
+        Donbury Style
+      </h1>
+
+      <Comida
+        source="../src/assets/Don/Arroz.png"
+        foodName="Arroz"
+        foodDescription="Arroz blanco aromatizada"
+        foodPrice="2.60"
+        gluten={true}
+        animacion={true}
+        right={true}
+      />
+
+      <Comida
+        source="../src/assets/Don/Karaage Don.png"
+        foodName="Karaage Don"
+        foodDescription="Pollo frito con arroz"
+        foodPrice="9.80"
+        gluten={true}
+        animacion={true}
+        hotSale={true}
+      />
+
+      <Comida
+        source="../src/assets/Don/Chicken Don.png"
+        foodName="Chicken Don"
+        foodDescription="Pollo con arroz"
+        foodPrice="9.80"
+        gluten={true}
+        animacion={true}
+        right={true}
+      />
+
+      <Comida
+        source="../src/assets/Don/Unagi Don.png"
+        foodName="Unagi Don"
+        foodDescription="Anguila con arroz"
+        foodPrice="13.80"
+        gluten={true}
+        animacion={true}
+      />
+
+      <Comida
+        source="../src/assets/Don/Pato Don.png"
+        foodName="Pato Don"
+        foodDescription="Pato con arroz"
+        foodPrice="9.80"
+        gluten={true}
+        animacion={true}
+        right={true}
+      />
+
+      <Comida
+        source="../src/assets/Don/Ebi Don.png"
+        foodName="Ebi Don"
+        foodDescription="Gambas con arroz"
+        foodPrice="9.80"
+        gluten={true}
+        animacion={true}
+      />
     </>
-  )
+  );
 }
 
-export default Don
+export default Don;
