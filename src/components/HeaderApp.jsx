a05d3d590d1cfbbd791fba9806b07c08e8cbcd7c
ende <1195562121ende@gmail.com>
2023-09-01 00:07:27 +0200

    Terminado el header de la aplicación, modificado y solucionado errores

--- src/components/HeaderApp.jsx
+++ src/components/HeaderApp.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 ("use cliente");
-import { Navbar } from "flowbite-react";
+// import { Navbar } from "flowbite-react";
 
-function HeaderApp() {
+function HeaderApp({ children }) {
+  const [IsOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const ocultarMenu = () => {
+      if (window.innerWidth > 960 && IsOpen) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("resize", ocultarMenu);
+
+    return () => {
+      window.addEventListener("resize", ocultarMenu);
+    };
+  });
+
+  const toggleOpen = () => {
+    setIsOpen(!IsOpen);
+  };
   return (
     <>
-      <Navbar fluid className="bg-neutral-950 w-screen">
-        {/* Logo con el link del home */}
+      <nav className="h-28 text-black relative shadow-sm bg-black w-screen">
+        {/* Logo principal del restaurante */}
         <Link
-          className="no-underline hover:brightness-125 hover:duration-300"
+          className="absolute top-2 left-4 no-underline hover:brightness-125 hover:duration-300"
           to="/"
         >
           <img
@@ -19,25 +37,15 @@ function HeaderApp() {
           />
         </Link>
 
-        {/* Logo del dropbar */}
-        <div className="flex md:order-2">
-          <Navbar.Toggle className="hover:duration-300" />
-        </div>
-
-        {/* Los  Elementos del menú*/}
-        <Navbar.Collapse>
+        {/* Enlaces de los elementos internos visibles */}
+        <div className="pr-6 hidden [&>a]:m-2 md:flex md:justify-center mt-4">
           <Link
             to="/Entrantes"
-            className="p-1 no-underline rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none md:shadow-md md:shadow-orange-300" //hidden md:block y esta guarrada
+            className="p-1 no-underline rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none md:shadow-md md:shadow-orange-300"
           >
             Entrantes
           </Link>
-          {/* <a
-            href="/Entrantes"
-            className= 'p-1 no-underline rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none md:shadow-md md:shadow-orange-300 md:hidden'
-          >
-            Entrantes
-            </a>  No sé si hacer esta guarrada*/}
+
           <Link
             to="/Ramen"
             className="p-1 no-underline  rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none md:shadow-md md:shadow-orange-300"
@@ -68,8 +76,75 @@ function HeaderApp() {
           >
             Idiomas
           </Link>
-        </Navbar.Collapse>
-      </Navbar>
+        </div>
+
+        {/* SVG del desplegable */}
+        <div
+          className="absolute md:hidden text-slate-200 hover:text-white hover:duration-200 w-auto right-4 top-10"
+          onClick={toggleOpen}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+            />
+          </svg>
+        </div>
+      </nav>
+
+      {/* Enlaces de los elementos internos escondidos de primera*/}
+      {IsOpen && (
+        <div
+          className="w-screen grid grid-rows-6 text-left items-center bg-black p-3"
+          onClick={toggleOpen}
+        >
+          <Link
+            to="/Entrantes"
+            className="p-1 no-underline rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none md:shadow-md md:shadow-orange-300"
+          >
+            Entrantes
+          </Link>
+
+          <Link
+            to="/Ramen"
+            className="p-1 no-underline  rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none md:shadow-md md:shadow-orange-300"
+          >
+            Ramen
+          </Link>
+          <Link
+            to="/Don"
+            className="p-1 no-underline  rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none lg:m-4 md:shadow-md md:shadow-orange-300"
+          >
+            Don Style
+          </Link>
+          <Link
+            to="/Combos"
+            className="p-1 no-underline  rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none lg:m-4 md:shadow-md md:shadow-orange-300"
+          >
+            Combos
+          </Link>
+          <Link
+            to="/BebidaPostre"
+            className="p-1 no-underline  rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-xl lg:text-3xl md:border-none lg:m-4 md:shadow-md md:shadow-orange-300"
+          >
+            Bebida y Postres
+          </Link>
+          <Link
+            to="/"
+            className="p-1 no-underline rounded-lg bg-transparent text-neutral-200 text-xl font-razha uppercase font-bold hover:duration-300 hover:text-amber-200 my-2 md:text-4xl md:borderc-none md:hidden md:shadow-md md:shadow-orange-300"
+          >
+            Idiomas
+          </Link>
+        </div>
+      )}
     </>
   );
 }
