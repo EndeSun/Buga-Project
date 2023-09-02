05a2ce5fbd212a7b01c304bfedb3c961979be83b
pjtc28 <133893146+pjtc28@users.noreply.github.com>
2023-09-02 22:15:51 +0200

    Componente Bebida iniciado, alergenos en comida puestos, precios revisados, sobre nosotros en movil esta bien falta hacerlo responsive.

--- src/components/BebidaComponent.jsx
+++ src/components/BebidaComponent.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function BebidaComponent() {
+function BebidaComponent({source, nombreBebida}) {
   return (
-    <div>BebidaComponent</div>
+    <>
+      <div className="flex flex-col items-center mr-5 ml-5">
+        <img src={source} alt={nombreBebida} className="max-w-full h-auto" />
+        <p className="mt-2 text-amber-600 text-center text-xl font-bold m-2 drop-shadow-2xl sm:text-3xl">{nombreBebida}</p>
+      </div>
+    </>
   )
 }
 
