d1faba35bcfe62d40811dec8cfb4f4b3b920aed2
ende <1195562121ende@gmail.com>
2023-09-04 00:26:08 +0200

    Componente bebida y la página bebida realizado

--- src/components/BebidaComponent.jsx
+++ src/components/BebidaComponent.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function BebidaComponent({source, nombreBebida}) {
+function BebidaComponent({ source, nombreBebida }) {
   return (
     <>
-      <div className="flex flex-col items-center mr-5 ml-5">
-        <img src={source} alt={nombreBebida} className="max-w-full h-auto" />
-        <p className="mt-2 text-amber-600 text-center text-xl font-bold m-2 drop-shadow-2xl sm:text-3xl">{nombreBebida}</p>
+      <div className="flex-col items-center justify-center border-4 border-slate-950 rounded-xl shadow-md shadow-teal-100 p-1 relative">
+        <img src={source} alt={nombreBebida} />
+        <p className=" text-amber-600 text-center text-md font-semibold m-2 drop-shadow-2xl font-caprasimo sm:text-3xla absolute bottom-0 sm:text-xl md:text-2xl lg:text-3xl">
+          {nombreBebida}
+        </p>
       </div>
     </>
-  )
+  );
 }
 
-export default BebidaComponent
+export default BebidaComponent;
