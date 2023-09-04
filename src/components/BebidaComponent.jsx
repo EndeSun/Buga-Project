2d30f5fcc7afec53c2cbe4ac1bf6474961db5542
ende <1195562121ende@gmail.com>
2023-09-04 02:30:52 +0200

    Página y componente bebida adaptado

--- src/components/BebidaComponent.jsx
+++ src/components/BebidaComponent.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {motion} from "framer-motion"
 
 function BebidaComponent({ source, nombreBebida }) {
   return (
     <>
-      <div className="flex-col items-center justify-center border-4 border-slate-950 rounded-xl shadow-md shadow-teal-100 p-1 relative">
-        <img src={source} alt={nombreBebida} />
+      <div className="flex-col items-center justify-center border-4 border-slate-950 rounded-xl shadow-md shadow-teal-100 p-1 relative overflow-hidden">
+        <motion.img src={source} alt={nombreBebida} whileHover={{scale: 1.3}} transition={{duration:0.3}}/>
         <p className=" text-amber-600 text-center text-md font-semibold m-2 drop-shadow-2xl font-caprasimo sm:text-3xla absolute bottom-0 sm:text-xl md:text-2xl lg:text-3xl">
           {nombreBebida}
         </p>
