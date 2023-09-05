016ec8e494deeec439bbd85603f4374e65222fee
ende <1195562121ende@gmail.com>
2023-09-05 19:22:22 +0200

    Arreglando error de import en el header y añadiendo detalles a bebidas

--- src/components/BebidaComponent.jsx
+++ src/components/BebidaComponent.jsx
@@ -4,10 +4,10 @@ import {motion} from "framer-motion"
 function BebidaComponent({ source, nombreBebida }) {
   return (
     <>
-      <div className="flex-col items-center justify-center border-4 border-slate-950 rounded-xl shadow-md shadow-teal-100 p-1 relative overflow-hidden">
-        <motion.img src={source} alt={nombreBebida} whileHover={{scale: 1.3}} transition={{duration:0.3}}/>
+      <div className="flex-col items-center justify-center border-4 border-slate-950 rounded-xl shadow-md shadow-teal-100 relative overflow-hidden ">
+        <motion.img src={source} alt={nombreBebida} initial={{scale:1.3}} whileHover={{scale: 1.5}} transition={{duration:0.3}}/>
         
-        <p className=" text-amber-600 text-center text-md font-semibold m-2 drop-shadow-2xl font-caprasimo sm:text-3xla absolute bottom-0 sm:text-xl md:text-2xl lg:text-3xl">
+        <p className=" text-white text-center text-md font-semibold m-2 drop-shadow-2xl font-caprasimo sm:text-3xla absolute bottom-0 left-0  sm:text-xl md:text-2xl lg:text-3xl">
           {nombreBebida}
         </p>
       </div>
