f75bac47adae639c8ea439d8e99ab82c9dc3a48a
ende <1195562121ende@gmail.com>
2023-09-06 17:22:56 +0200

    Terminando el componente bebida

--- src/pages/Carta/BebidaPostre.jsx
+++ src/pages/Carta/BebidaPostre.jsx
@@ -3,71 +3,72 @@ import BebidaComponent from "../../components/BebidaComponent";
 
 function BebidaPostre() {
   return (
-    <>
-      <h1 className="text-center font-bold font-caprasimo text-2xl md:text-3xl lg:text-4xl text-amber-500 my-4">
+    <section className="bg-black">
+      <h1 className="text-center font-bold font-caprasimo text-2xl md:text-3xl lg:text-4xl text-amber-500 mb-8">
         Bebidas
       </h1>
 
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-4 mx-12 sm:mx-14 md:mx-16">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-1 lg:grid-cols-4 mx-12 sm:mx-14 md:mx-1">
         <BebidaComponent
           source="../src/assets/Bebidas/vichy.webp"
-          nombreBebida="Vichy 300 mL"
+          nombreBebida="Vichy"
+          
         />
         <BebidaComponent
           source="../src/assets/Bebidas/cola.webp"
-          nombreBebida="Coca Cola 350 mL"
+          nombreBebida="Coca Cola"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/colaZero.webp"
-          nombreBebida="Coca Cola Zero 350 mL"
+          nombreBebida="Coca Cola Zero"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/colaZeroZero.webp"
-          nombreBebida="Coca Cola Zero Zero 350 mL"
+          nombreBebida="Coca Cola Zero Zero"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/fantaNaranja.webp"
-          nombreBebida="Fanta Naranja 350 mL"
+          nombreBebida="Fanta Naranja"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/fantaLimon.webp"
-          nombreBebida="Fanta Limón 350 mL"
+          nombreBebida="Fanta Limón"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/aquariusNaranja.webp"
-          nombreBebida="Aquarius Naranja 300 mL"
+          nombreBebida="Aquarius Naranja"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/aquariusLimon.webp"
-          nombreBebida="Aquarius Limón 300 mL"
+          nombreBebida="Aquarius Limón"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/nestea.webp"
-          nombreBebida="Fanta Limón 300 mL"
+          nombreBebida="Fanta Limón"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ramuneLimon.webp"
-          nombreBebida="Ramune Limón 200 mL"
+          nombreBebida="Ramune Limón"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ramuneMelon.webp"
-          nombreBebida="Ramune Melón 200 mL"
+          nombreBebida="Ramune Melón"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ramunePinya.webp"
-          nombreBebida="Ramune Piña 200 mL"
+          nombreBebida="Ramune Piña"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ramuneFresa.webp"
-          nombreBebida="Ramune Fresa 200 mL"
+          nombreBebida="Ramune Fresa"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ramuneLichy.webp"
-          nombreBebida="Ramune Lichy 200 mL"
+          nombreBebida="Ramune Lichy"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/te.webp"
-          nombreBebida="Té verde 500 mL"
+          nombreBebida="Té verde"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/agua.webp"
@@ -87,27 +88,27 @@ function BebidaPostre() {
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ambar.webp"
-          nombreBebida="Copa ambar"
+          nombreBebida="Copa Ambar"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/ambarLimon.webp"
-          nombreBebida="Copa ambar con limón"
+          nombreBebida="Copa Ambar con Limón"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/copaBlanco.webp"
-          nombreBebida="Copa vino blanco"
+          nombreBebida="Copa Vino Blanco"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/copaTinto.webp"
-          nombreBebida="Copa vino tinto"
+          nombreBebida="Copa Vino Tinto"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/botellaBlanco.webp"
-          nombreBebida="Botella vino blanco"
+          nombreBebida="Botella Vino Blanco"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/botellaTinto.webp"
-          nombreBebida="Botella vino tinto"
+          nombreBebida="Botella Vino Tinto"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/chupitoSake.webp"
@@ -119,21 +120,19 @@ function BebidaPostre() {
         />
         <BebidaComponent
           source="../src/assets/Bebidas/chupitoAfrutado.webp"
-          nombreBebida="Chupito afrutado"
+          nombreBebida="Chupito Afrutado"
         />
         <BebidaComponent
           source="../src/assets/Bebidas/chupitoSoju.webp"
-          nombreBebida="Soju coreano 350 mL"
+          nombreBebida="Soju Coreano"
         />
-
-
-        {/* Agrega más instancias de BebidaComponent según sea necesario */}
       </div>
 
-      <h1 className="text-center font-bold font-caprasimo text-2xl text-amber-500 md:text-3xl lg:text-4xl my-4">
+      <h1 className="text-center font-bold font-caprasimo text-2xl text-amber-500 md:text-3xl lg:text-4x my-2">
         Postres
       </h1>
-    </>
+
+    </section>
   );
 }
 
