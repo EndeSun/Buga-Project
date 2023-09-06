ed352fae61d0e602a1808c01b173aca95992a6a5
ende <1195562121ende@gmail.com>
2023-09-06 17:22:56 +0200

    Terminando el componente bebida

--- src/components/BebidaComponent.jsx
+++ src/components/BebidaComponent.jsx
@@ -3,15 +3,15 @@ import {motion} from "framer-motion"
 
 function BebidaComponent({ source, nombreBebida }) {
   return (
-    <>
-      <div className="flex-col items-center justify-center border-4 border-slate-950 rounded-xl shadow-md shadow-teal-100 relative overflow-hidden ">
-        <motion.img src={source} alt={nombreBebida} initial={{scale:1.3}} whileHover={{scale: 1.5}} transition={{duration:0.3}}/>
+    <div className="flex justify-center">
+      <div className="rounded-xl shadow-md relative overflow-hidden w-40 md:w-48 lg:w-72 md:my-4">
+        <motion.img src={source} alt={nombreBebida} initial={{scale:1.3}} whileHover={{scale: 1.5}} transition={{duration:0.3}} className="w-full"/>
         
-        <p className=" text-white text-center text-md font-semibold m-2 drop-shadow-2xl font-caprasimo sm:text-3xla absolute bottom-0 left-0  sm:text-xl md:text-2xl lg:text-3xl">
+        <p className=" text-white text-left text-md md:text-xl lg:text-3xl font-bold m-1 drop-shadow-2xl font-caprasimo absolute top-0 left-0">
           {nombreBebida}
         </p>
       </div>
-    </>
+    </div>
   );
 }
 
